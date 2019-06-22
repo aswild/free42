9e3ad50a8595bfb11d6e3c5eba4793e391f7f197
Allen Wild <allenwild93@gmail.com>
2019-06-21 20:17:25 -0400

    get Intel Math Library to build
      * Change hard-coded OSX paths to hard-coded Linux paths (assume
        Android NDK is in /opt/android-ndk, like in the AUR package)
      * Unify the four different patches into one since they're basically
        all the same.
      * Fix fexpect_t and wchar_t re-definitions on the newer NDK clang
        versions

--- android/app/src/main/cpp/build-intel-lib.sh
+++ android/app/src/main/cpp/build-intel-lib.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
-NDK="$HOME/Library/Android/sdk/ndk-bundle"
-export PATH="`/bin/pwd`/bin:$NDK/prebuilt/darwin-x86_64/bin:$NDK/toolchains/llvm/prebuilt/darwin-x86_64/bin:$PATH"
+set -xe
+
+NDK="/opt/android-ndk"
+export PATH="$PWD/bin:$NDK/prebuilt/linux-x86_64/bin:$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH"
 BUILT=0
+
+make_wrapper() {
+    cat >$2 <<EOF
+#!/bin/sh
+set -x
+$1 "\$@"
+EOF
+    chmod +x $2
+}
 
 build_arch () {
     ARCH=$1
@@ -13,10 +24,14 @@ build_arch () {
         return
     fi
     rm -rf bin IntelRDFPMathLib20U1
-    mkdir bin
-    echo "$ARCH_CC"' "$@"' > bin/gcc
-    echo "$ARCH_AR"' "$@"' > bin/ar
-    chmod +x bin/*
+    mkdir -p bin
+    # Intel library doesn't like cross-compiling, it's makefiles are a clusterfuck
+    # of functions and lists and abstractions, and so CC *must* be exactly "gcc"
+    # or one of the other supported compilers. Thus, we need wrappers that call
+    # the correct cross compiler.
+    make_wrapper "$ARCH_CC" bin/gcc
+    make_wrapper "$ARCH_AR" bin/ar
+
     if [ -f ../../../../../inteldecimal/IntelRDFPMathLib20U1.tar.gz ]
     then
         tar xfz ../../../../../inteldecimal/IntelRDFPMathLib20U1.tar.gz
@@ -25,9 +40,9 @@ build_arch () {
     fi
 
     cd IntelRDFPMathLib20U1
-    patch -p0 <../intel-lib-android-$ARCH.patch
+    patch -p1 <../intel-lib-android.patch
     cd LIBRARY
-    make CC=gcc CALL_BY_REF=1 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=0 _HOST_OS=Linux
+    make CC=gcc CALL_BY_REF=1 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=0 _HOST_OS=Linux lib
     mv libbid.a ../../$ARCH_LIB
     cd ../..
     BUILT=1
