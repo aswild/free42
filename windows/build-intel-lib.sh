11e541e312881e758e15fbbddb4e1e90ef3ef9a5
Allen Wild <allenwild93@gmail.com>
2019-07-10 01:39:36 -0400

    windows: start of mingw build framework
    Update build-intel-lib.sh and make link-files.sh to work in MSYS MinGW x64
    
    Unsurprisingly, the Intel lib makefiles are wacky and don't understand
    mingw (GNU toolchains on Windows) so override some variables to get the
    compile and archive commands and file extensions correct.

--- windows/build-intel-lib.sh
+++ windows/build-intel-lib.sh
@@ -1,11 +1,36 @@
-#!/bin/sh -e
-if [ -f cl111libbid.lib ]; then exit 0; fi
-tar xvfz ../inteldecimal/IntelRDFPMathLib20U1.tar.gz
+#!/bin/bash
+
+set -e
+if [[ "$MSYSTEM" == MINGW* ]]; then
+    MINGW=1
+    LIB=libbid.a
+    LIB_OUT=libbid-mingw.a
+else
+    MINGW=0
+    LIB=libbid.lib
+    LIB_OUT=cl111libbid.lib
+fi
+
+if [ -f $LIB_OUT ]; then exit 0; fi
+
+set -x
+tar xfz ../inteldecimal/IntelRDFPMathLib20U1.tar.gz
 cd IntelRDFPMathLib20U1
-patch -p0 <../intel-lib-windows-32bit.patch
-cd LIBRARY
-cmd /c ..\\..\\build-intel-lib.bat
-mv libbid.lib ../../cl111libbid.lib
+if (( $MINGW )); then
+    patch -p0 --binary -i ../intel-lib-windows-32bit.patch
+    cd LIBRARY
+    # the second line of variables is to hack up Intel's insane makefiles that
+    # don't understand mingw GNU toolchains on Windows.
+    make CALL_BY_REF=1 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=0 \
+         _HOST_OS=Windows_NT CC=gcc AR_CMD='ar rcs' AR_OUT= O=o A=a \
+         lib
+else
+    patch -p0 <../intel-lib-windows-32bit.patch
+    cd LIBRARY
+    cmd /c ..\\..\\build-intel-lib.bat
+fi
+
+mv $LIB ../../$LIB_OUT
 cd ../..
 ( echo '#ifdef FREE42_FPTEST'; echo 'const char *readtest_lines[] = {'; tr -d '\r' < IntelRDFPMathLib20U1/TESTS/readtest.in | sed 's/^\(.*\)$/"\1",/'; echo '0 };'; echo '#endif' ) > readtest_lines.cpp
 cp IntelRDFPMathLib20U1/TESTS/readtest.c .
