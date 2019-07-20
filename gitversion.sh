d96ca995edb48f96c05e71bdfca1587fb958b0f0
Allen Wild <allenwild93@gmail.com>
2019-07-20 18:55:17 -0400

    make gitversion.sh common, not just windows only
    and add a --full flag which will prefix with the main version.

--- gitversion.sh
+++ gitversion.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+cd "`dirname $0`"
 rev="`git rev-parse --short HEAD`"
 dirty=
 if [ -n "`git status --porcelain --untracked=no`" ]; then
     dirty=+
 fi
-echo "${rev}${dirty}"
+if [ "$1" = "--full" ]; then
+    mainversion="`cat VERSION`-"
+fi
+echo "${mainversion}${rev}${dirty}"
