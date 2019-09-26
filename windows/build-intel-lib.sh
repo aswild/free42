3de238f333040802647c8425aa0e9a7c868145be
Allen Wild <allenwild93@gmail.com>
2019-09-25 20:07:52 -0400

    windows: fix build if MAKEFLAGS is set
    Apparently nmake reads MAKEFLAGS from the environment (which gets passed
    form the msys shell to cmd.exe), but it's not happy with GNU options
    like '-j8'.
    
    Fix by explicitly unsetting MAKEFLAGS in build-intel-lib.sh. Clean up
    more of that script for readability while I'm here.

--- windows/build-intel-lib.sh
+++ windows/build-intel-lib.sh
@@ -1,11 +1,29 @@
-#!/bin/sh -xe
-if [ -f cl111libbid.lib ]; then exit 0; fi
+#!/bin/sh
+set -eo pipefail
+
+# don't let GNU MAKEFLAGS from MSYS environment confuse nmake
+unset MAKEFLAGS
+
+if [ -f cl111libbid.lib ]; then
+    echo "cl111libbid.lib already built"
+    exit 0
+fi
+
+set -x
 tar xzf ../inteldecimal/IntelRDFPMathLib20U1.tar.gz
 cd IntelRDFPMathLib20U1
 patch -p0 --binary <../intel-lib-windows-32bit.patch
+
 cd LIBRARY
 cmd //c ..\\..\\build-intel-lib.bat
 mv libbid.lib ../../cl111libbid.lib
+
 cd ../..
-( echo '#ifdef FREE42_FPTEST'; echo 'const char *readtest_lines[] = {'; tr -d '\r' < IntelRDFPMathLib20U1/TESTS/readtest.in | sed 's/^\(.*\)$/"\1",/'; echo '0 };'; echo '#endif' ) > readtest_lines.cpp
 cp IntelRDFPMathLib20U1/TESTS/readtest.c .
+(
+    echo '#ifdef FREE42_FPTEST'
+    echo 'const char *readtest_lines[] = {'
+    tr -d '\r' < IntelRDFPMathLib20U1/TESTS/readtest.in | sed 's/^\(.*\)$/"\1",/'
+    echo 'NULL };'
+    echo '#endif'
+) > readtest_lines.cpp
